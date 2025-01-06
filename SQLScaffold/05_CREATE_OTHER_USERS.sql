979d38346236c9715e158703b872f267da66258e
vis256 <67900555+vis256@users.noreply.github.com>
2025-01-06 10:41:29 +0100

    Fix roles

--- SQLScaffold/05_CREATE_OTHER_USERS.sql
+++ SQLScaffold/05_CREATE_OTHER_USERS.sql
@@ -1,3 +1,3 @@
-INSERT INTO User VALUES(1, 'ash', 'y6AEWitJtwSjq80Mlgxh9g==', 1, '42SviIww4sUoZZ66tUJw0oVH4Ev/6gyzAIKPoyg0jpk=', 'Ash Ketchum');
-INSERT INTO User VALUES(2, 'misty', 'y6AEWitJtwSjq80Mlgxh9g==', 1, '42SviIww4sUoZZ66tUJw0oVH4Ev/6gyzAIKPoyg0jpk=', 'Misty Waterflower');
-INSERT INTO User VALUES(3, 'brock', 'y6AEWitJtwSjq80Mlgxh9g==', 1, '42SviIww4sUoZZ66tUJw0oVH4Ev/6gyzAIKPoyg0jpk=', 'Brock Harrison');
+INSERT INTO User VALUES(1, 'ash', 'y6AEWitJtwSjq80Mlgxh9g==', 0, '42SviIww4sUoZZ66tUJw0oVH4Ev/6gyzAIKPoyg0jpk=', 'Ash Ketchum');
+INSERT INTO User VALUES(2, 'misty', 'y6AEWitJtwSjq80Mlgxh9g==', 0, '42SviIww4sUoZZ66tUJw0oVH4Ev/6gyzAIKPoyg0jpk=', 'Misty Waterflower');
+INSERT INTO User VALUES(3, 'brock', 'y6AEWitJtwSjq80Mlgxh9g==', 0, '42SviIww4sUoZZ66tUJw0oVH4Ev/6gyzAIKPoyg0jpk=', 'Brock Harrison');
